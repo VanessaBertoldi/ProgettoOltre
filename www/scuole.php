75ad90a4c7339e71fdca9efa73d6e98ce46230fe
muhammadhamzasajjad <45758804+muhammadhamzasajjad@users.noreply.github.com>
2019-02-10 14:20:14 +0100

    Update scuole.php

--- www/scuole.php
+++ www/scuole.php
@@ -72,21 +72,24 @@
 		<div class="top-menu">
 			<div class="container">
 				<div class="row">
-					<div class="col-xs-2">
-						<div id="fh5co-logo"><a href="index.php"><img src="images/logo6.PNG" style="width:200px"></a></div>
-					</div>
-					<div class="col-xs-10 text-right menu-1">
+					<div class="col-xs-11 text-right menu-1">
 						<ul>
-							<li><a href="index.php">Home</a></li>
-							<li><a href="partecipare.php">Come partecipare</a></li>
-							<li class="active"><a href="scuole.php">Scuole</a></li>
-                            <li><a href="organizzazioni.php">Organizzazioni</a></li>
-                            <li><a href="progetti.php">I nostri progetti</a></li>
-                            <li><a href="gallery.php">Gallery</a></li>
-							
-							<li><a href="contact.php">Contact</a></li>
-							<li class="btn-cta"><a href="#"><span>Login</span></a></li>
-							<li class="btn-cta"><a href="#"><span>Sign Up</span></a></li>
+							<li class="active"><a href="index.php">Home</a></li>
+							<li class="has-dropdown">
+								<a href="partecipare.php">Come partecipare</a>
+								<ul class="dropdown">
+									<li><a href="scuole.php">Scuole</a></li>
+									<li><a href="partners.php">Partners</a></li>
+									<li><a href="studenti.php">Studenti</a></li>
+								</ul>
+							</li>
+							<li><a href="scuole.php">Scuole</a></li>
+							<li><a href="organizzazioni.php">Organizzazioni</a></li>
+							<li><a href="progetti.php">I nostri progetti</a></li>
+							<li><a href="contact.php">Contatti</a></li>
+							<li><a href="gallery.php">Galleria</a></li>
+							<li class="btn-cta"><a href="login.php"><span>Login</span></a></li>
+							<li class="btn-cta"><a href="registrati.php"><span>Registrati</span></a></li>
 						</ul>
 					</div>
 				</div>
@@ -113,58 +116,89 @@
 	  	</div>
 	</aside>
 
-	<div id="fh5co-project">
+	<div id="fh5co-practice" class="fh5co-bg-section">
 		<div class="container">
 			<div class="row animate-box">
 				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
-					<h2>Won Cases</h2>
+					<h2>Our Legal Practice Area</h2>
 					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
 				</div>
 			</div>
-		</div>
-		<div class="container">
 			<div class="row">
-				<div class="col-md-4 col-sm-6 text-center fh5co-project animate-box" data-animate-effect="fadeIn">
-					<a href="#"><img src="images/project-1.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-						<h3>Business</h3>
-						<span>CEO. Hon Doe</span>
-					</a>
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-home"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Real Estate Law</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
 				</div>
-				<div class="col-md-4 col-sm-6 text-center fh5co-project animate-box" data-animate-effect="fadeIn">
-					<a href="#"><img src="images/project-2.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-						<h3>Legal Advice</h3>
-						<span>Atty. John Doe</span>
-					</a>
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-eye"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Insurance Law</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
 				</div>
-				<div class="col-md-4 col-sm-6 text-center fh5co-project animate-box" data-animate-effect="fadeIn">
-					<a href="#"><img src="images/project-3.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-						<h3>Read Bible</h3>
-						<span>Ptr. Jhon Doe</span>
-					</a>
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-shopping-cart"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Business Law</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
 				</div>
-				<div class="col-md-4 col-sm-6 text-center fh5co-project animate-box" data-animate-effect="fadeIn">
-					<a href="#"><img src="images/project-4.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-						<h3>Affidavit</h3>
-						<span>Atty. Boo Doe</span>
-					</a>
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-umbrella"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Personal Injury</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
 				</div>
-				<div class="col-md-4 col-sm-6 text-center fh5co-project animate-box" data-animate-effect="fadeIn">
-					<a href="#"><img src="images/project-5.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-						<h3>Sports</h3>
-						<span>Atty. Smith D</span>
-					</a>
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-heart"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Medical Neglegence</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
 				</div>
-				<div class="col-md-4 col-sm-6 text-center fh5co-project animate-box" data-animate-effect="fadeIn">
-					<a href="#"><img src="images/project-6.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-						<h3>Photography</h3>
-						<span>Phtr. Arnt Tee</span>
-					</a>
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-help"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Criminal Defense</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-12 text-center animate-box">
+					<p><a class="btn btn-primary btn-lg btn-learn" href="#">View More</a></p>
 				</div>
 			</div>
 		</div>
 	</div>
 
-	<div id="fh5co-testimonial" class="fh5co-bg-section">
+	<div id="fh5co-testimonial">
 		<div class="container">
 			<div class="row animate-box">
 				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
@@ -237,36 +271,38 @@
 		<div class="container">
 			<div class="row row-pb-md">
 				<div class="col-md-3 fh5co-widget">
-					<h4>Attorney's Law</h4>
-					<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta adipisci architecto culpa amet.</p>
+					<h4>Andare Oltre</h4>
+					<p>L’Istituto di Formazione Professionale Alberghiero di Rovereto è ospitato in un edificio risalente al 1904 sorto come Istituto Pellagrologico. </p>
 				</div>
 				<div class="col-md-3 col-md-push-1">
-					<h4>Navigation</h4>
+					<h4>Navigazione</h4>
 					<ul class="fh5co-footer-links">
-						<li><a href="#">Home</a></li>
-						<li><a href="#">Practice Areas</a></li>
-						<li><a href="#">Won Cases</a></li>
-						<li><a href="#">Blog</a></li>
-						<li><a href="#">About us</a></li>
+						<li class="active"><a href="index.php">Home</a></li>
+						<li>
+							<a href="partecipare.php">Come partecipare</a>
+							<ul class="dropdown">
+								<li><a href="scuole.php">Scuole</a></li>
+								<li><a href="partners.php">Partners</a></li>
+								<li><a href="studenti.php">Studenti</a></li>
+							</ul>
+						</li>
+						<li><a href="scuole.php">Scuole</a></li>
+						<li><a href="organizzazioni.php">Organizzazioni</a></li>
+						<li><a href="progetti.php">I nostri progetti</a></li>
+						<li><a href="contact.php">Contatti</a></li>
+						<li><a href="gallery.php">Galleria</a></li>
+						<li class="btn-cta"><a href="login.php"><span>Login</span></a></li>
+						<li class="btn-cta"><a href="registrati.php"><span>Registrati</span></a></li>
 					</ul>
 				</div>
 
 				<div class="col-md-3 col-md-push-1">
-					<h4>Contact Information</h4>
+					<h4>Contatti</h4>
 					<ul class="fh5co-footer-links">
-						<li>198 West 21th Street, <br> Suite 721 New York NY 10016</li>
-						<li><a href="tel://1234567920">+ 1235 2355 98</a></li>
-						<li><a href="mailto:info@yoursite.com">info@yoursite.com</a></li>
+						<li>Viale dei Colli, 17, 38068 Rovereto TN</li>
+						<li><a href="tel://+39 0464 439164">+39 0464 439164</a></li>
+						<li><a href="mailto:andareoltre.com">info@yoursite.com</a></li>
 						<li><a href="http://gettemplates.co">gettemplates.co</a></li>
-					</ul>
-				</div>
-
-				<div class="col-md-3 col-md-push-1">
-					<h4>Opening Hours</h4>
-					<ul class="fh5co-footer-links">
-						<li>Mon - Thu: 9:00 - 21 00</li>
-						<li>Fri 8:00 - 21 00</li>
-						<li>Sat 9:30 - 15: 00</li>
 					</ul>
 				</div>
 
@@ -275,15 +311,15 @@
 			<div class="row copyright">
 				<div class="col-md-12 text-center">
 					<p>
-						<small class="block">&copy; 2016 Free HTML5. All Rights Reserved.</small> 
-						<small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a></small>
+						<small class="block">&copy;Tutti i diritti riservati.</small> 
+						<small class="block">Designed by <a href="http://buonarroti.tn.it/" target="_blank">I.T.T. Buonarroti</a> Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a></small>
 					</p>
 					<p>
 						<ul class="fh5co-social-icons">
 							<li><a href="#"><i class="icon-twitter"></i></a></li>
-							<li><a href="#"><i class="icon-facebook"></i></a></li>
-							<li><a href="#"><i class="icon-linkedin"></i></a></li>
-							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+						<li><a href="#"><i class="icon-facebook"></i></a></li>
+						<li><a href="#"><i class="icon-linkedin"></i></a></li>
+						<li><a href="#"><i class="icon-dribbble"></i></a></li>
 						</ul>
 					</p>
 				</div>
