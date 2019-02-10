198ed9afba71bfc74468c7c9a370c3b0f4181d5d
muhammadhamzasajjad <45758804+muhammadhamzasajjad@users.noreply.github.com>
2019-02-10 14:12:44 +0100

    Update contact.php

--- www/contact.php
+++ www/contact.php
@@ -72,20 +72,24 @@
 		<div class="top-menu">
 			<div class="container">
 				<div class="row">
-					<div class="col-xs-2">
-						<div id="fh5co-logo"><a href="index.php"><img src="images/logo6.PNG" style="width:200px"></a></div>
-					</div>
-					<div class="col-xs-10 text-right menu-1">
+					<div class="col-xs-11 text-right menu-1">
 						<ul>
-							<li><a href="index.php">Home</a></li>
-							<li><a href="partecipare.php">Come partecipare</a></li>
+							<li class="active"><a href="index.php">Home</a></li>
+							<li class="has-dropdown">
+								<a href="come_partecipare.php">Come partecipare</a>
+								<ul class="dropdown">
+									<li><a href="scuole.php">Scuole</a></li>
+									<li><a href="partners.php">Partners</a></li>
+									<li><a href="studenti.php">Studenti</a></li>
+								</ul>
+							</li>
 							<li><a href="scuole.php">Scuole</a></li>
-							<li><a href="organizzazioni.php">Organizzazioni</a></li>
-                            <li><a href="progetti.php">I nostri progetti</a></li>
-                            <li><a href="gallery.php">Gallery</a></li>
-							<li class="active"><a href="contact.php">Contact</a></li>
-							<li class="btn-cta"><a href="#"><span>Login</span></a></li>
-							<li class="btn-cta"><a href="#"><span>Sign Up</span></a></li>
+							<li><a href="organizzazione.php">Organizzazioni</a></li>
+							<li><a href="i_nostri_progetti.php">I nostri progetti</a></li>
+							<li><a href="contact.php">Contatti</a></li>
+							<li><a href="gallery.php">Galleria</a></li>
+							<li class="btn-cta"><a href="login.php"><span>Login</span></a></li>
+							<li class="btn-cta"><a href="registrati.php"><span>Registrati</span></a></li>
 						</ul>
 					</div>
 				</div>
@@ -102,7 +106,7 @@
 		   		<div class="container">
 		   			<div class="col-md-10 col-md-offset-1 text-center js-fullheight slider-text">
 		   				<div class="slider-text-inner desc">
-		   					<h2 class="heading-section">Contact Us</h2>
+		   					<h2 class="heading-section">Practicing Area</h2>
 		   					<p class="fh5co-lead">Designed with <i class="icon-heart3"></i> by the fine folks at <a href="http://freehtml5.co" target="_blank">FreeHTML5.co</a></p>
 		   				</div>
 		   			</div>
@@ -112,67 +116,138 @@
 	  	</div>
 	</aside>
 
-	<div id="fh5co-contact">
+	<div id="fh5co-practice" class="fh5co-bg-section">
 		<div class="container">
-			<div class="row">
-				<div class="col-md-5 col-md-push-1 animate-box">
-					
-					<div class="fh5co-contact-info">
-						<h3>Contact Information</h3>
-						<ul>
-							<li class="address">198 West 21th Street, <br> Suite 721 New York NY 10016</li>
-							<li class="phone"><a href="tel://1234567920">+ 1235 2355 98</a></li>
-							<li class="email"><a href="mailto:info@yoursite.com">info@yoursite.com</a></li>
-							<li class="url"><a href="http://gettemplates.co">gettemplates.co</a></li>
-						</ul>
-					</div>
-
-				</div>
-				<div class="col-md-6 animate-box">
-					<h3>Send A Message</h3>
-					<form action="#">
-						<div class="row form-group">
-							<div class="col-md-6">
-								<!-- <label for="fname">First Name</label> -->
-								<input type="text" id="fname" class="form-control" placeholder="Your firstname">
-							</div>
-							<div class="col-md-6">
-								<!-- <label for="lname">Last Name</label> -->
-								<input type="text" id="lname" class="form-control" placeholder="Your lastname">
-							</div>
-						</div>
-
-						<div class="row form-group">
-							<div class="col-md-12">
-								<!-- <label for="email">Email</label> -->
-								<input type="text" id="email" class="form-control" placeholder="Your email address">
-							</div>
-						</div>
-
-						<div class="row form-group">
-							<div class="col-md-12">
-								<!-- <label for="subject">Subject</label> -->
-								<input type="text" id="subject" class="form-control" placeholder="Your subject of this message">
-							</div>
-						</div>
-
-						<div class="row form-group">
-							<div class="col-md-12">
-								<!-- <label for="message">Message</label> -->
-								<textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Say something about us"></textarea>
-							</div>
-						</div>
-						<div class="form-group">
-							<input type="submit" value="Send Message" class="btn btn-primary">
-						</div>
-
-					</form>		
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Our Legal Practice Area</h2>
+					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
 				</div>
 			</div>
-			
+			<div class="row">
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-home"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Real Estate Law</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-eye"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Insurance Law</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-shopping-cart"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Business Law</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-umbrella"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Personal Injury</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-heart"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Medical Neglegence</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-help"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Criminal Defense</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-12 text-center animate-box">
+					<p><a class="btn btn-primary btn-lg btn-learn" href="#">View More</a></p>
+				</div>
+			</div>
 		</div>
 	</div>
-	<div id="map" class="fh5co-map"></div>
+
+	<div id="fh5co-testimonial">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+					<h2>Testimonials</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1">
+					<div class="row animate-box">
+						<div class="owl-carousel owl-carousel-fullwidth">
+							<div class="item">
+								<div class="testimony-slide active text-center">
+									<figure>
+										<img src="images/user-1.jpg" alt="user">
+									</figure>
+									<span>Jean Doe, via <a href="#" class="twitter">Twitter</a></span>
+									<blockquote>
+										<p>&ldquo;Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.&rdquo;</p>
+									</blockquote>
+								</div>
+							</div>
+							<div class="item">
+								<div class="testimony-slide active text-center">
+									<figure>
+										<img src="images/user-1.jpg" alt="user">
+									</figure>
+									<span>John Doe, via <a href="#" class="twitter">Twitter</a></span>
+									<blockquote>
+										<p>&ldquo;Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.&rdquo;</p>
+									</blockquote>
+								</div>
+							</div>
+							<div class="item">
+								<div class="testimony-slide active text-center">
+									<figure>
+										<img src="images/user-1.jpg" alt="user">
+									</figure>
+									<span>John Doe, via <a href="#" class="twitter">Twitter</a></span>
+									<blockquote>
+										<p>&ldquo;Far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.&rdquo;</p>
+									</blockquote>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 
 	<div id="fh5co-started" style="background-image:url(images/img_bg_2.jpg);">
@@ -196,36 +271,38 @@
 		<div class="container">
 			<div class="row row-pb-md">
 				<div class="col-md-3 fh5co-widget">
-					<h4>Attorney's Law</h4>
-					<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta adipisci architecto culpa amet.</p>
+					<h4>Andare Oltre</h4>
+					<p>L’Istituto di Formazione Professionale Alberghiero di Rovereto è ospitato in un edificio risalente al 1904 sorto come Istituto Pellagrologico. </p>
 				</div>
 				<div class="col-md-3 col-md-push-1">
-					<h4>Navigation</h4>
+					<h4>Navigazione</h4>
 					<ul class="fh5co-footer-links">
-						<li><a href="#">Home</a></li>
-						<li><a href="#">Practice Areas</a></li>
-						<li><a href="#">Won Cases</a></li>
-						<li><a href="#">Blog</a></li>
-						<li><a href="#">About us</a></li>
+						<li class="active"><a href="index.php">Home</a></li>
+						<li>
+							<a href="come_partecipare.php">Come partecipare</a>
+							<ul class="dropdown">
+								<li><a href="scuole.php">Scuole</a></li>
+								<li><a href="partners.php">Partners</a></li>
+								<li><a href="studenti.php">Studenti</a></li>
+							</ul>
+						</li>
+						<li><a href="scuole.php">Scuole</a></li>
+						<li><a href="organizzazione.php">Organizzazioni</a></li>
+						<li><a href="i_nostri_progetti.php">I nostri progetti</a></li>
+						<li><a href="contact.php">Contatti</a></li>
+						<li><a href="gallery.php">Galleria</a></li>
+						<li class="btn-cta"><a href="login.php"><span>Login</span></a></li>
+						<li class="btn-cta"><a href="registrati.php"><span>Registrati</span></a></li>
 					</ul>
 				</div>
 
 				<div class="col-md-3 col-md-push-1">
-					<h4>Contact Information</h4>
+					<h4>Contatti</h4>
 					<ul class="fh5co-footer-links">
-						<li>198 West 21th Street, <br> Suite 721 New York NY 10016</li>
-						<li><a href="tel://1234567920">+ 1235 2355 98</a></li>
-						<li><a href="mailto:info@yoursite.com">info@yoursite.com</a></li>
+						<li>Viale dei Colli, 17, 38068 Rovereto TN</li>
+						<li><a href="tel://+39 0464 439164">+39 0464 439164</a></li>
+						<li><a href="mailto:andareoltre.com">info@yoursite.com</a></li>
 						<li><a href="http://gettemplates.co">gettemplates.co</a></li>
-					</ul>
-				</div>
-
-				<div class="col-md-3 col-md-push-1">
-					<h4>Opening Hours</h4>
-					<ul class="fh5co-footer-links">
-						<li>Mon - Thu: 9:00 - 21 00</li>
-						<li>Fri 8:00 - 21 00</li>
-						<li>Sat 9:30 - 15: 00</li>
 					</ul>
 				</div>
 
@@ -234,15 +311,15 @@
 			<div class="row copyright">
 				<div class="col-md-12 text-center">
 					<p>
-						<small class="block">&copy; 2016 Free HTML5. All Rights Reserved.</small> 
-						<small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a></small>
+						<small class="block">&copy;Tutti i diritti riservati.</small> 
+						<small class="block">Designed by <a href="http://buonarroti.tn.it/" target="_blank">I.T.T. Buonarroti</a> Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a></small>
 					</p>
 					<p>
 						<ul class="fh5co-social-icons">
 							<li><a href="#"><i class="icon-twitter"></i></a></li>
-							<li><a href="#"><i class="icon-facebook"></i></a></li>
-							<li><a href="#"><i class="icon-linkedin"></i></a></li>
-							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+						<li><a href="#"><i class="icon-facebook"></i></a></li>
+						<li><a href="#"><i class="icon-linkedin"></i></a></li>
+						<li><a href="#"><i class="icon-dribbble"></i></a></li>
 						</ul>
 					</p>
 				</div>
@@ -275,9 +352,6 @@
 	<!-- Magnific Popup -->
 	<script src="js/jquery.magnific-popup.min.js"></script>
 	<script src="js/magnific-popup-options.js"></script>
-	<!-- Google Map -->
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
-	<script src="js/google_map.js"></script>
 	<!-- Main -->
 	<script src="js/main.js"></script>
 
