87801c69bacea02b139ee4f0f42d2a210fade9c7
TommasoZajac <45758739+TommasoZajac@users.noreply.github.com>
2019-02-12 10:15:10 +0100

    tolto del codice che forse dava un errore

--- www/js/popupLogin.js
+++ www/js/popupLogin.js
@@ -1,14 +1,5 @@
-// Get the modal
-var modal = document.getElementById('idLogin');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
 function openPopup(){
-	modal.style.display='block';
+	document.getElementById('idLogin').style.display='block';
 	if ( $('body').hasClass('offcanvas') ) {
     	$('body').removeClass('offcanvas');
     	$('.js-fh5co-nav-toggle').removeClass('active');
